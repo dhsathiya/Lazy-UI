3cf858b22fa26595cc34871acc76d9f0e4e31aa1
dhsathiya <devarshisathiya5@gmail.com>
2019-07-28 21:09:37 +0530

    Add dynamic environment support

--- ssh/dialog.sh
+++ ssh/dialog.sh
@@ -2,16 +2,14 @@
 
 #declare -a AVAILABLE_OPTIONS
 declare -a ENVIRONMENTS
-ENVIRONMENTS=(Prod Stag Dev)
+ENVIRONMENTS=($(ls $HOME/.lazy_ssh/))
 #AVAILABLE_OPTIONS=(Add Edit Delete)
 program_stage=0
-#SSH_INTO=$(dialog --stdout --menu "select option" 10 100 20 rtcamp "" gk "" vanguard "")
-#echo "$SSH_INTO"
 
 function server_select {
     while IFS= read -r line; do
         DIALOG_STRING_SERVER="$DIALOG_STRING_SERVER "$line" |"
-    done < "$1"
+    done < "$HOME/.lazy_ssh/$1"
     SSH_SERVER=$(dialog --stdout --menu "select option" 20 100 50 $DIALOG_STRING_SERVER)
     clear
     ssh root@$SSH_SERVER
